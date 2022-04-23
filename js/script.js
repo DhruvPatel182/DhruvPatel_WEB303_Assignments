057f46b4146422574379a0e84912a50e25e9a1d9
DhruvPatel182 <91303351+DhruvPatel182@users.noreply.github.com>
2022-04-23 03:14:56 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(function () { 
+    
+    var $tbody = $('tbody');
+    var $search = $('#filter-search');
+    var cache = [
+        {
+            Element: $row0Reference,
+            firstName : "Moiraine",
+            lastNameFirstCharacter : "s"
+        },
+        {
+            Element: $row1Reference,
+            firstName : "Egwane",
+            lastNameFirstCharacter : "a"
+        },{
+            Element: $row2Reference,
+            firstName : "Perrin",
+            lastNameFirstCharacter : "a"
+        },{
+            Element: $row3Reference,
+            firstName : "Tam",
+            lastNameFirstCharacter : "a"
+        },{
+            Element: $row4Reference,
+            firstName : "Ihvon",
+            lastNameFirstCharacter : "s"
+        },{
+            Element: $row5Reference,
+            firstName : "Rand",
+            lastNameFirstCharacter : "a"
+        },{
+            Element: $row6Reference,
+            firstName : "Aram",
+            lastNameFirstCharacter : "t"
+        },{
+            Element: $row7Reference,
+            firstName : "Logain",
+            lastNameFirstCharacter : "d"
+        },
+    ]
+    
+   if(lastNameFirstCharacter === a){
+       var row1 = $('<tr></tr>');
+        $row.append(<td></td>).text();
+   }
+
+   if(lastNameFirstCharacter === s){
+
+   }
+   if(lastNameFirstCharacter === t){
+
+   }
+   if(lastNameFirstCharacter === d){
+       
+   }
+    
+ })
